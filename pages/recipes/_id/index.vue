ab52cc92fdc700145fd6821b16cff1bca0a1baeb
Leonardo <lvneves2010@gmail.com>
2020-05-14 13:17:20 -0300

    simple app

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="single-recipe">
+        <h1> {{ recipe.title }} </h1>
+        <div>
+            <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
+        </div>
+        <p> {{ recipe.descriptionText }} </p>
+    </section>
+</template>
+
+<script>
+export default {
+    asyncData(context) {
+        return new Promise((res, err) => {
+            setTimeout(() => {
+                res({
+                    recipe: [
+                        {
+                            id:'1',
+                            title: 'Feijoada',
+                            previewText: 'receita de feijoada fácil e rápida',
+                            descriptionText: 'Aqui vai a receita completa',
+                            thumbnail: 'https://redesuldenoticias.com.br/content/uploads/2018/05/feijoada-receita-1024x651.jpg'
+                        },
+                        {
+                            id:'2',
+                            title: 'Macarrão ao Vongole',
+                            previewText: 'Deliciosa receita de macarrão com frutos do mar',
+                            descriptionText: 'Aqui vai a receita completa',
+                            thumbnail: 'https://www.comidaereceitas.com.br/wp-content/uploads/2010/07/espaguete_molho_vongole.jpg'
+                        }
+                    ].find(el => el.id === context.params.id)
+                })
+            }, 1500 )
+        } )
+    },
+
+}
+</script>
+
+<style scoped>
+.single-recipe {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+
+.recipe-image {
+    width: 40%;
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="recipes">
+      <Recipe
+      v-for="recipe in recipes" :key="recipe.id"
+      :id="recipe.id"
+      :title="recipe.title"
+      :previewText="recipe.previewText"
+      :thumbnail ="recipe.thumbnail"  />
+  </section>
+</template>
+
+<script>
+import Recipe from '../../components/Recipe'
+
+export default {
+    components: { Recipe },
+    asyncData() {
+        return new Promise((res, err) => {
+            setTimeout(() => {
+                res({
+                    recipes: [
+                        {
+                            id:'1',
+                            title: 'Feijoada',
+                            previewText: 'receita de feijoada fácil e rápida',
+                            thumbnail: 'https://redesuldenoticias.com.br/content/uploads/2018/05/feijoada-receita-1024x651.jpg'
+                        },
+                        {
+                            id:'2',
+                            title: 'Macarrão ao Vongole',
+                            previewText: 'Deliciosa receita de macarrão com frutos do mar',
+                            thumbnail: 'https://www.comidaereceitas.com.br/wp-content/uploads/2010/07/espaguete_molho_vongole.jpg'
+                        }
+                    ]
+                })
+            }, 1500 )
+        } )
+    },
+}
+</script>
+
+<style scoped>
+.recipes {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+</style>
